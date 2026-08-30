b747037ab68e798d1627da4267e74fed9fae66cd
paydii <193906237+paydii@users.noreply.github.com>
2026-08-30 20:48:33 +0200

    Move scope to getsigit

--- npm/sigit/src/index.ts
+++ npm/sigit/src/index.ts
@@ -22,7 +22,7 @@ function getExePath() {
   try {
     // Since the binary will be located inside node_modules, we can simply call require.resolve
     return require.resolve(
-      `@smbcloud/sigit-${os}-${arch}/bin/sigit${extension}`,
+      `@getsigit/sigit-${os}-${arch}/bin/sigit${extension}`,
     );
   } catch (e) {
     throw new Error(
